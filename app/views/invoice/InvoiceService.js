6ccd64fed289585d9387777e21ed5969479d96a1
amacoerp <deekshitha.cse5235@gmail.com>
2021-12-30 12:09:53 +0530

    Update InvoiceService.js

--- app/views/invoice/InvoiceService.js
+++ app/views/invoice/InvoiceService.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a4d63eed53d795c8885f527f35f1603e415311a2227ee42b33e98d1c39fd8ba
-size 5391
+oid sha256:b394ae66fa8ae4f9e89bdd205d9f696c2d53a38cc4c57ec6f94f8d2003652c58
+size 5351
